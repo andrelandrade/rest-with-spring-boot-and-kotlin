4174af3c532a83f858fe5fb855b471dbdbc3ed95
André Luis de Andrade <al.andrade256@gmail.com>
2022-12-15 06:48:53 -0300

    Adding POST, PUT and DELETE http methods

--- rest-with-spring-boot-and-kotlin/src/main/kotlin/br/com/example/services/PersonService.kt
+++ rest-with-spring-boot-and-kotlin/src/main/kotlin/br/com/example/services/PersonService.kt
@@ -10,6 +10,18 @@ class PersonService {
     private val counter: AtomicLong = AtomicLong()
     private val logger = Logger.getLogger(PersonService::class.java.name)
 
+    fun findAll(): List<Person> {
+        logger.info("Finding all people!")
+
+        val persons: MutableList<Person> = ArrayList()
+        for (i in 0..7) {
+            val person = mockPerson(i)
+            persons.add(person)
+        }
+
+        return persons
+    }
+
     fun findById(id: Long): Person {
         logger.info("Finding one person!")
 
@@ -18,6 +30,22 @@ class PersonService {
         person.firstName = "John"
         person.lastName = "Doe"
         person.address = "far far away"
+        return person
+    }
+
+    fun create(person: Person) = person
+
+    fun update(person: Person) = person
+
+    fun delete(id: Long) {}
+
+    private fun mockPerson(i: Int): Person {
+        val person = Person()
+        person.id = counter.incrementAndGet()
+        person.firstName = "Person Name $i"
+        person.lastName = "Last Name $i"
+        person.address = "far far away $i"
+
         return person
     }
 }
