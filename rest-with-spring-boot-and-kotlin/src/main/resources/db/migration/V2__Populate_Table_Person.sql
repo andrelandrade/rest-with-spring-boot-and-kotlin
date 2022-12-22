56b4e9684fd2f2c4214dce8a6f058de138851c0d
André Luis de Andrade <al.andrade256@gmail.com>
2022-12-22 06:40:59 -0300

    Adding Flyway migrations

---
+++ rest-with-spring-boot-and-kotlin/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`) VALUES
+	(1, 'Ayrton', 'Senna', 'São Paulo'),
+	(2, 'Leonardo', 'da Vinci', 'Anchiano - Italy'),
+	(4, 'Indira', 'Gandhi', 'Porbandar - India'),
+	(5, 'Mahatma', 'Gandhi', 'Porbandar - India'),
+	(7, 'Muhammad', 'Ali', 'Kentucky - United States'),
+	(9, 'Nelson', 'Mandela', 'Mvezo - South Africa'),
+	(10, 'Nikola', 'Tesla', 'Smiljan - Croatia');
