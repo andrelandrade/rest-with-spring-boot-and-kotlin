373fb0088c20f305502edb23bff6a4078f3f35af
André Luis de Andrade <al.andrade256@gmail.com>
2022-12-22 06:49:33 -0300

    Custon JSON Serialization

--- rest-with-spring-boot-and-kotlin/src/main/kotlin/br/com/example/data/vo/v1/PersonVO.kt
+++ rest-with-spring-boot-and-kotlin/src/main/kotlin/br/com/example/data/vo/v1/PersonVO.kt
@@ -1,7 +1,18 @@
 package br.com.example.data.vo.v1
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+@JsonPropertyOrder("id", "address", "firstName", "lastName")
 data class PersonVO (
+    @field:JsonIgnore
     var id: Long = 0,
+
+    @field:JsonProperty("first_name")
     var firstName: String = "",
+
+    @field:JsonProperty("last_name")
     var lastName: String = "",
     var address: String = "",
 )
